4055ce75f8c6625f2f314c14dd40c1bc00647cf8
KomendaKacper <komenda@student.agh.edu.pl>
2024-08-14 22:01:56 +0200

    RestCrudUpdate

--- dev-spring-boot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ dev-spring-boot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.luv2code.demo.rest;
 
 import entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +13,44 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    // define @PostConstruct to load the student data ... onlu once!
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Barbara", "Rossi"));
         theStudents.add(new Student("Mario", "Luigiano"));
+    }
 
+    // define endpoint for "/students" - return a list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
     }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        //check the studentId against list size
+        if  ((studentId < 0) || (studentId >= theStudents.size())) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
+    }
 }
+
+
+
+
+
+
+
