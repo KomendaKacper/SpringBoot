27b4beab542ab628bbba10171ccec225ed22a49b
KomendaKacper <komenda@student.agh.edu.pl>
2024-08-23 14:20:18 +0200

    @ManyToMany

---
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
@@ -0,0 +1,32 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Course;
+import com.luv2code.cruddemo.entity.Instructor;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save (Instructor theInstrutor);
+
+    Instructor findInstructor (int theId);
+
+    void deleteInstructorById (int theId);
+
+    List<Course> findCoursesByInstructorId (int theId);
+
+    Instructor findInstructorByIdJoinFetch (int theId);
+
+    void update(Instructor theInstrutor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById (int theId);
+
+    void deleteCourseById (int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
+
+    Course findCourseAndStudentsByCourseId(int theId);
+}
