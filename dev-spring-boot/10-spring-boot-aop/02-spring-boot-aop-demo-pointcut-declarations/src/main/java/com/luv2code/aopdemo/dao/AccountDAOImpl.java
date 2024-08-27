da241c937c24b5afba0f22fbc0beda261e5e5364
KomendaKacper <komenda@student.agh.edu.pl>
2024-08-27 12:38:07 +0200

    aop finish

--- dev-spring-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
+++ dev-spring-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
@@ -3,6 +3,9 @@ package com.luv2code.aopdemo.dao;
 import com.luv2code.aopdemo.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Repository // For being visible for component scanning
 public class AccountDAOImpl implements AccountDAO{
@@ -10,6 +13,34 @@ public class AccountDAOImpl implements AccountDAO{
     private String name;
 
     private String serviceCode;
+
+    @Override
+    public List<Account> findAccounts() {
+        return findAccounts(false);
+    }
+
+    @Override
+    public List<Account> findAccounts(boolean tripWire) {
+
+        // for academic purposes ... simulate an exception
+        if (tripWire == true) {
+            throw new RuntimeException("No soup for you!!!");
+        }
+
+        List<Account> myAccounts = new ArrayList<Account>();
+
+        // create sample accounts
+        Account temp1 = new Account("John", "Silver");
+        Account temp2 = new Account("Madhu", "Platinium");
+        Account temp3 = new Account("Luca", "Gold");
+
+        // add to our accounts list
+        myAccounts.add(temp1);
+        myAccounts.add(temp2);
+        myAccounts.add(temp3);
+
+        return myAccounts;
+    }
 
     @Override
     public void addAccount(Account theAccount, boolean vipFlag) {
